7d551253ff472851399f09fd30d492e52bf34e70
xsf <xsf_uestc_ncl@163.com>
2019-04-26 11:45:05 +0800

    @feat  task test

--- learntransform/src/main/groovy/me/xsfdev/learntransform/GetExtension.groovy
+++ learntransform/src/main/groovy/me/xsfdev/learntransform/GetExtension.groovy
@@ -1,6 +1,10 @@
 package me.xsfdev.learntransform
 
 import com.android.build.gradle.AppExtension
+import com.android.build.gradle.BaseExtension
+import com.android.build.gradle.api.ApplicationVariant
+import com.android.build.gradle.api.BaseVariant
+import com.android.build.gradle.internal.api.ApplicationVariantImpl
 import me.xsfdev.learntransform.extension.SExtension
 import me.xsfdev.learntransform.extension.Student
 import org.gradle.api.Plugin
@@ -23,7 +27,7 @@ class GetExtension implements Plugin<Project> {
         project.extensions.create('student', Student)
 
         //定义一个task读取build.gradle中的'se'和'student'的属性
-        project.task('readExtension') << {
+        project.task('readExtension').doLast {
             def se = project['se']
             def student = project['student']
             println "The SExtension's myName is " + se.myName
@@ -31,10 +35,33 @@ class GetExtension implements Plugin<Project> {
             println "The student's phone is " + student.phone
         }
 
+        project.getPlugins().each {
+            println "plugin:${it.class.name}"
+        }
+
+        //或者 直接 project.android
+        BaseExtension extension = project.extensions.getByName("android")
+        def android = project.extensions.getByType(AppExtension)
+        project.android
+
+        android.applicationVariants
+        //可以看出就是遍历集合把对象传递给闭包。               
+        android.applicationVariants.all {
+            variant ->
+                def appID = variant.generateBuildConfig.appPackageName
+                def variantData = variant.variantData
+                def scope = variantData.scope
+                String generateBuildConfigTaskName = variant.getVariantData().getScope().getGenerateBuildConfigTask().name
+                println "appID:" + appID
+                println "scope:" + scope
+                println "generateBuildConfigTaskName:" + generateBuildConfigTaskName
+        }
+
+
         //读取android属性
         //注册Transform
-//        def android = project.extensions.findByType(AppExtension)
-//        android.registerTransform(new PreDex(project))
+        //def android = project.extensions.findByType(AppExtension)
+        //android.registerTransform(new PreDex(project))
 
     }
 }
